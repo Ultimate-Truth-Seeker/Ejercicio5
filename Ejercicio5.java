fa5feee29b8a5d5fb306708f0207c307dc97d3b1
Ultimate-Truth-Seeker <96703353+Ultimate-Truth-Seeker@users.noreply.github.com>
2023-10-18 23:52:39 -0600

    1

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -7,8 +7,9 @@ import java.util.List;
 import java.util.Scanner;
 /**
  * Programa para aplicación de jugadores de volleyball
+ * Se cargan datos desde un csv al inicio y al salir da opción de guardar los datos del programa en otro o el mismo csv
  * NOTA: se asume que la primera línea contiene datos de un jugador y no nombres del tipo de dato
- * NOTA: solo se carga un archivo al inicio y se guarda al final
+ * NOTA: Hay que compilarlo desde la carpeta donde está el programa, sino no compila. Usar % java Ejercicio5
  * 
  * @author Ultimate-Truth-Seeker
  * @version 13-10-2023
@@ -17,10 +18,10 @@ public class Ejercicio5 {
     public static void main(String[] args) {
         List<Player> players = new ArrayList<>();
         Scanner s = new Scanner(System.in);
-        System.out.println("Bienvenido, ingrese la ubicación de su csv con datos de jugadores");
+        System.out.println("Bienvenido, ingrese el nombre de su csv con datos de jugadores");
         String path = s.nextLine();
         boolean menu = true;
-        try (Scanner rd = new Scanner(new File(path))){
+        try (Scanner rd = new Scanner(new File(path))){// Escaneo del archivo csv, catch si ocurre un fallo
             while (rd.hasNextLine()) {
                 Scanner sc = new Scanner(rd.nextLine());
                 sc.useDelimiter(",");
@@ -55,16 +56,16 @@ public class Ejercicio5 {
                 }
             }
             
-        } catch (Exception FileNotFoundException) {
+        } catch (Exception FileNotFoundException) {// Error de lectura
             System.out.println("Error al leer archivo, revise que todos los datos estén correctos\nIngresando a menú...");
         }
-        while (menu) {
+        while (menu) {// Ingresa a menú independiente de si leyo o no el documento
             System.out.println("Bienvenido, elija una opción:\n1.Añadir jugador al sistema\n2.Mostrar todos los jugadores inscritos\n3.Mejores jugadores\n4.Guardar en csv y salir");
             int op = s.nextInt();
             switch (op) {
                 default:
                 break;
-                case 1:
+                case 1:// añadir un jugador más al programa
                 System.out.println("Ingrese el nombre del jugador:");
                 s.nextLine();
                 String name = s.nextLine();
@@ -85,7 +86,7 @@ public class Ejercicio5 {
                 while (type < 1 && type > 4) {
                     type = s.nextInt();
                 }
-                switch (type) {
+                switch (type) {// Campos específicos para cada tipo de jugador
                     case 1:
                     System.out.println("Ingresse las capturas efectivas del líbero:");
                     int effectiveCatch = s.nextInt();
@@ -124,29 +125,34 @@ public class Ejercicio5 {
                     break;
                 }
                 break;
-                case 2:
+                case 2:// Listado de jugadores uno por uno
                 System.out.println("Listado de jugadores");
                 for (Player ply : players) {
+                    System.out.println("Nombre: "+ ply.getName() +", País: " + ply.getCountry()+", Fallos: "+ply.getFails()+", Aces: "+ply.getAces()+", Servicios: "+ply.getServices());
                     if (ply.getClass() == OppositeAuxiliar.class) {
                         OppositeAuxiliar oa = (OppositeAuxiliar) ply;
-                        System.out.println(ply.getName() +"," + ply.getCountry()+","+ply.getFails()+","+ply.getAces()+","+ply.getServices()+","+oa.isOA()+","+oa.getAtacks()+","+oa.getBloqued()+","+oa.getFailedBlock());
+                        if (oa.isOA()) {
+                            System.out.println("Clase: Opuesto, Ataques: "+oa.getAtacks()+", Bloqueos efectivos: "+oa.getBloqued()+", Bloqueos fallidos: "+oa.getFailedBlock());
+                        } else {
+                            System.out.println("Clase: Auxiliar, Ataques: "+oa.getAtacks()+", Bloqueos efectivos: "+oa.getBloqued()+", Bloqueos fallidos: "+oa.getFailedBlock());
+                        }
                     } else if (ply.getClass() == Feinter.class) {
                         Feinter f = (Feinter) ply;
-                        System.out.println(ply.getName() +"," + ply.getCountry()+","+ply.getFails()+","+ply.getAces()+","+ply.getServices()+","+f.getPassings()+","+f.getFeints());
+                        System.out.println("Clase: Pasador, Pases: "+f.getPassings()+", Fintas: "+f.getFeints());
                     } else if (ply.getClass() == Sweeper.class) {
                         Sweeper sw = (Sweeper) ply;
-                        System.out.println(ply.getName() +"," + ply.getCountry()+","+ply.getFails()+","+ply.getAces()+","+ply.getServices()+","+sw.getEffectiveCatch());
+                        System.out.println("Clase: Líbero, Capturas efectivas: "+sw.getEffectiveCatch());
                     }
                     System.out.println("Efectividad: " + ply.efectivity());
                 }
                 break;
-                case 3:
+                case 3:// Pasadores con efectividad > 80
                 System.out.println("Pasadores con más de 80% de efectividad");
                 for (Player ply : players) {
                     if (ply.getClass() == Feinter.class && ply.efectivity() > 80) {
                         System.out.println(ply.getName() + ": " + ply.efectivity()+"%");   
                     }
-                }
+                }// 3 Líberos con mayor efectividad
                 System.out.println("Los tres mejores Líberos:");
                 int[] indexes = new int[3]; indexes[0] = -1; indexes[1] = -1; indexes[2] = -1;
                 for (int x = 0; x < 3; x++) {
@@ -170,13 +176,13 @@ public class Ejercicio5 {
                 }
                 System.out.println("(La efectividad va de -100% a 200%)");
                 break;
-                case 4:
+                case 4:// Salida
                 menu = false;
-                System.out.println("Ingrese el nombre del archivo en que desea guardar los datos o deje en blanco para no guardar. Añada .csv al final.");
+                System.out.println("Ingrese el nombre del archivo en que desea guardar los datos o deje en blanco para no guardar. La extensión .csv se añade automáticamente");
                 s.nextLine();
                 path = s.nextLine();
-                if (path.equals("") == false) {
-                    try (FileWriter wr = new FileWriter(new File(path))) {
+                if (path.equals("") == false) {//Si se ingreso nombre de archivo a crear escribir datos en el archivo
+                    try (FileWriter wr = new FileWriter(new File(path + ".csv"))) {
                         for (Player ply : players) {
                             if (ply.getClass() == OppositeAuxiliar.class) {
                                 OppositeAuxiliar oa = (OppositeAuxiliar) ply;
